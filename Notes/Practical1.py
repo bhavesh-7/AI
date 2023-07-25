68235f761ba47aebdddc29db6fa6e05843a8816b
Bhavesh Mishra <91950960+bhavesh-7@users.noreply.github.com>
2023-07-25 03:01:20 -0700

    Create Practical1.py

---
+++ Notes/Practical1.py
@@ -0,0 +1,17 @@
+graph = {'A':set(['B','C']),
+         'B': set(['A','D','E']),
+         'C': set(['A','F']),
+         'D': set(['B']),
+         'E': set(['B','F']),
+         'F': set(['C','E'])}
+#print(graph)
+
+def dfs(graph,start):
+  visited, stack = [], [start]
+  while stack:
+    vertex = stack.pop()
+    if vertex not in visited:
+      visited.append(vertex)
+      stack.extend(graph[vertex] - set(visited))
+  return visited
+dfs(graph,'A')
