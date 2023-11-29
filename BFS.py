0b569d626a8911f220e217e855301aa8de95bad8
Bhavesh Mishra <91950960+bhavesh-7@users.noreply.github.com>
2023-11-29 13:21:53 +0530

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -1,10 +1,10 @@
-graph = {'A': set(['B','C']),
-'B' : set(['A','D','E']),
-'C' : set(['A','F']),
-'D' : set(['B']),
-'E' : set(['B','F']),
-'F' : set(['C','E'])}
-
+graph = {'A': set(['B', 'C']),
+'B': set(['A', 'D', 'E']),
+'C': set(['A', 'F']),
+'D': set(['B']),
+'E': set(['B', 'F']),
+'F': set(['C', 'E'])}
+print("Graph : ",graph);
 def bfs(graph,start):
   visited, queue =[],[start]
   while queue:
@@ -13,5 +13,23 @@ def bfs(graph,start):
       visited.append(vertex)
       queue.extend(graph[vertex] - set(visited))
   return visited
+print("BFS : ",bfs(graph, 'A'))
 
-bfs(graph,'A')
+def bfs_paths(graph, start, goal):
+  queue = [(start, [start])]
+  while queue:
+    (vertex, path) = queue.pop(0)
+    for next in graph[vertex] - set(path):
+      if next == goal:
+        yield path + [next]
+      else:
+        queue.append((next, path + [next]))
+
+print("Paths (A,F): ",list(bfs_paths(graph, 'A', 'F')))
+def shortest_path(graph, start, goal):
+  try:
+    return next(bfs_paths(graph, start, goal))
+  except StopIteration:
+    return None
+
+print("Shortest Path: ",shortest_path(graph, 'A', 'F'))
